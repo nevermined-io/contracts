2939930d6044f9bbefb648294e0ef5713659921a
Pedro Gutiérrez <pedrogp9693@gmail.com>
2020-04-29 11:26:01 +0200

    Upgrade version

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/keyko-io/nevermind-contracts',
-    version='0.1.2',
+    version='0.1.3',
     zip_safe=False,
 )
 
