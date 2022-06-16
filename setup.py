886266ede293275857749d654e7a11afb273f783
Aitor <1726644+aaitor@users.noreply.github.com>
2022-06-16 15:38:45 +0200

    Adding Token name and symbol as potential parameters for ERC-721 implementations (#328)
    * Adding Token name and symbol as potential parameters for ERC-721 implementations
    
    * fixing tests
    
    * typo

--- setup.py
+++ setup.py
@@ -39,6 +39,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/nevermined-io/contracts',
-    version='2.0.0',
+    version='2.0.1',
     zip_safe=False,
 )
