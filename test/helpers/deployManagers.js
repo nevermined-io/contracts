6e0010a363bf7380d92470fec671dcd0944f40be
Sami Mäkelä <mrsmkl@gmail.com>
2022-07-12 18:45:58 +0300

    fixing this again

--- test/helpers/deployManagers.js
+++ test/helpers/deployManagers.js
@@ -12,7 +12,7 @@ const deployManagers = async function(deployer, owner, governor = owner) {
     const token = await testUtils.deploy('NeverminedToken', [owner, owner], deployer)
     const nvmConfig = await testUtils.deploy('NeverminedConfig', [owner, governor, false], deployer)
     const nft = await testUtils.deploy('NFTUpgradeable', [''], deployer)
-    const nft721 = await testUtils.deploy('NFT721Upgradeable', ['NFT721', 'NVM'], deployer)
+    const nft721 = await testUtils.deploy('NFT721Upgradeable', [], deployer)
 
     const didRegistry = await testUtils.deploy('DIDRegistry', [owner, nft.address, nft721.address, nvmConfig.address, constants.address.zero], deployer, [didRegistryLibrary])
     const royaltyManager = await testUtils.deploy('StandardRoyalties', [didRegistry.address], deployer)
