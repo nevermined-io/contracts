86770e165bf05b3cc3314570a3860c08604dbde0
Aitor <1726644+aaitor@users.noreply.github.com>
2022-07-15 14:05:49 +0200

    setting as `markdown` knowing prints in text format

--- scripts/run-mythril-local.sh
+++ scripts/run-mythril-local.sh
@@ -8,7 +8,7 @@ rm -f $REPORT_FILE
 
 for CONTRACT in $FLATTENED_FOLDER/*.sol; do
   printf "Processing $CONTRACT\n"
-  myth a $CONTRACT -o text --execution-timeout $TIMEOUT -t 5 2>> $REPORT_FILE
+  myth a $CONTRACT -o markdown --execution-timeout $TIMEOUT -t 5 2>> $REPORT_FILE
 done
 
 printf "\e[32m✔ Mythril analysis done, report file created: $REPORT_FILE.\e[0m\n"
