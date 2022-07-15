f921f077dcbb9f7b3629ef419376938268d11aee
Aitor <1726644+aaitor@users.noreply.github.com>
2022-07-15 14:03:32 +0200

    cleanup

--- scripts/run-mythril.sh
+++ scripts/run-mythril.sh
@@ -11,5 +11,3 @@ for i in artifacts/*.external.json; do
   echo $i
   myth a -a $(jq -r .address $i) --rpc localhost:18545 -o text --execution-timeout 30 -t 5 2>> mythril_report.txt
 done
-
-#myth a flattened/AgreementStoreManager.sol -o text --execution-timeout 30 -t 5 2>> mythril_report.txt
