22b8c4915778ca552baa0eab579022970dd3f43c
Aitor <1726644+aaitor@users.noreply.github.com>
2022-07-25 12:11:39 +0200

    bumping to `v2.0.2`

--- setup.py
+++ setup.py
@@ -39,6 +39,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/nevermined-io/contracts',
-    version='2.0.1',
+    version='2.0.2',
     zip_safe=False,
 )
