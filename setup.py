010e2206b52eb2c370e133e9519058c30c374edf
Javier Cortejoso <javi@clabs.co>
2022-07-29 15:16:59 +0200

    Bump version to 2.0.4

--- setup.py
+++ setup.py
@@ -39,6 +39,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/nevermined-io/contracts',
-    version='2.0.3',
+    version='2.0.4',
     zip_safe=False,
 )
