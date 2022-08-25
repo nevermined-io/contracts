5f8576ee7d310b807868e91c346ce1986180afa7
Sami Mäkelä <mrsmkl@gmail.com>
2022-08-25 12:25:41 +0300

    trying these versions ...

--- scripts/build-circuit.sh
+++ scripts/build-circuit.sh
@@ -6,7 +6,7 @@ wget https://hermez.s3-eu-west-1.amazonaws.com/powersOfTau28_hez_final_14.ptau
 
 git clone https://github.com/iden3/circom.git
 cd circom
-git checkout v2.0.7
+git checkout v2.0.6
 cargo install --path circom
 cd -
 
