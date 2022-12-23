140a7e0c9bcfb772cdb18a6c22d282404f6a6c86
Aitor <1726644+aaitor@users.noreply.github.com>
2022-12-23 19:27:34 +0100

    rebuilding `develop` docker image

--- scripts/build-circuit.sh
+++ scripts/build-circuit.sh
@@ -1,11 +1,11 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 rm contracts/verifier.sol
 
 wget https://hermez.s3-eu-west-1.amazonaws.com/powersOfTau28_hez_final_14.ptau
 wget https://github.com/iden3/circom/releases/download/v2.0.6/circom-linux-amd64
 
-chmod 755 circom-linux-amd64
+chmod +x circom-linux-amd64
 ./circom-linux-amd64 circuits/keytransfer.circom --r1cs --wasm --sym
 
 mv keytransfer.r1cs circuits
