4a4e82c7b9f9ad6c30c716c73eb8d1589324e60d
Sami Mäkelä <mrsmkl@gmail.com>
2023-01-24 14:17:40 +0200

    opengsn setup should now work

--- scripts/deploy/setupContracts.js
+++ scripts/deploy/setupContracts.js
@@ -528,11 +528,19 @@ async function setupContracts({
     }
 
     if (addresses.stage < 23) {
-        console.log('Setting up OpenGSN forwarder')
+        const chainId = await web3.eth.getChainId()
+        console.log(`Setting up OpenGSN forwarder for chain ID ${chainId}`)
+        if (!gsn && process.env.OPENGSN_FORWARDER) {
+            gsn = process.env.OPENGSN_FORWARDER
+        } else if (chainId === 80001) {
+            gsn = '0x4d4581c01A457925410cd3877d17b2fd4553b2C5' // mumbai
+        } else if (chainId === 1) {
+            gsn = '0xAa3E82b4c4093b4bA13Cb5714382C99ADBf750cA' // ethereum mainnet
+        } else if (chainId === 137) {
+            gsn = '0xdA78a11FD57aF7be2eDD804840eA7f4c2A38801d' // polygon
+        }
         if (testnet && gsn) {
             await callContract(artifacts.NeverminedConfig, a => a.setTrustedForwarder(gsn))
-        } else if (process.env.OPENGSN_FORWARDER) {
-            await callContract(artifacts.NeverminedConfig, a => a.setTrustedForwarder(process.env.OPENGSN_FORWARDER))
         } else {
             console.warn('Warning, OPENGSN_FORWARDER environment variable is not set. Meta transactions will not work')
         }
