2541e91eeda3ed7c7cc8cc20c8bdc520603330af
Sami Mäkelä <mrsmkl@gmail.com>
2023-03-30 10:17:15 +0300

    adding contract for access with dleq

---
+++ contracts/conditions/AccessDLEQCondition.sol
@@ -0,0 +1,145 @@
+pragma solidity ^0.8.0;
+// Copyright 2022 Nevermined AG.
+// SPDX-License-Identifier: (Apache-2.0 AND CC-BY-4.0)
+// Code is Apache-2.0 and docs are CC-BY-4.0
+
+
+import './Condition.sol';
+import '../registry/DIDRegistry.sol';
+import '../interfaces/IAccessControl.sol';
+import '../agreements/AgreementStoreManager.sol';
+import '../libraries/Bn128.sol';
+
+/**
+ * @title Access Condition with DLEQ transfer proof
+ * @author Nevermined
+ *
+ * @dev Implementation of the Access Condition with transfer proof (DLEQ).
+ * 
+ */
+contract AccessDLEQCondition is Condition {
+
+    bytes32 constant public CONDITION_TYPE = keccak256('AccessDLEQCondition');
+
+    AgreementStoreManager private agreementStoreManager;
+
+    event Fulfilled(
+        bytes32 indexed _agreementId,
+        uint _cipher,
+        uint[2] _secretId,
+        uint[2] _buyer,
+        uint[2] _provider,
+        uint[2] _reencrypt,
+        uint[2] _proof,
+        bytes32 _conditionId
+    );
+    
+   /**
+    * @notice initialize init the 
+    *       contract with the following parameters
+    * @dev this function is called only once during the contract
+    *       initialization.
+    * @param _owner contract's owner account address
+    * @param _conditionStoreManagerAddress condition store manager address
+    * @param _agreementStoreManagerAddress agreement store manager address
+    */
+    function initialize(
+        address _owner,
+        address _conditionStoreManagerAddress,
+        address _agreementStoreManagerAddress
+    )
+        external
+        initializer()
+    {
+        OwnableUpgradeable.__Ownable_init();
+        transferOwnership(_owner);
+
+        conditionStoreManager = ConditionStoreManager(
+            _conditionStoreManagerAddress
+        );
+
+        agreementStoreManager = AgreementStoreManager(
+            _agreementStoreManagerAddress
+        );
+    }
+
+   /**
+    * @notice hashValues generates the hash of condition inputs 
+    *        with the following parameters
+    * @param _cipher is the encrypted key
+    * @param _buyer buyer public key
+    * @param _provider provider public key
+    * @param _secretId public key for the secret used in encryption
+    * @return bytes32 hash of all these values 
+    */
+    function hashValues(
+        uint _cipher,
+        uint[2] memory _secretId,
+        uint[2] memory _provider,
+        uint[2] memory _buyer
+    )
+        public
+        pure
+        returns (bytes32)
+    {
+        return keccak256(abi.encode(_cipher, _secretId[0], _secretId[1], _provider[0], _provider[1], _buyer[0], _buyer[1]));
+    }
+
+   /**
+    * @notice fulfill key transfer
+    * @dev The key with hash _origHash is transferred to the _buyer from _provider.
+    * @param _agreementId associated agreement
+    * @param _buyer buyer public key
+    * @param _provider provider public key
+    * @param _cipher encrypted version of the key
+    * @param _reencrypt Re-encryption key from provider to buyer
+    * @param _proof DLEQ proof of correctness
+    * @return condition state (Fulfilled/Aborted)
+    */
+    function fulfill(
+        bytes32 _agreementId,
+        uint _cipher,
+        uint[2] memory _secretId,
+        uint[2] memory _provider,
+        uint[2] memory _buyer,
+        uint[2] memory _reencrypt,
+        uint[2] memory _proof
+    )
+        public
+        returns (ConditionStoreLibrary.ConditionState)
+    {
+        bytes32 _id = generateId(
+            _agreementId,
+            hashValues(_cipher, _secretId, _provider, _buyer)
+        );
+
+
+        G1Point memory _rebase = Bn128.g1Add(G1P(_buyer), G1P(_secretId));
+        // check the dleq proof
+        Bn128.dleqverify(Bn128.g1(), _rebase, G1P(_provider), G1P(_reencrypt), DleqProof(_proof[0], _proof[1]), _id);
+
+        ConditionStoreLibrary.ConditionState state = super.fulfill(
+            _id,
+            ConditionStoreLibrary.ConditionState.Fulfilled
+        );
+
+        emit Fulfilled(
+            _agreementId,
+            _cipher,
+            _secretId,
+            _buyer,
+            _provider,
+            _reencrypt,
+            _proof,
+            _id
+        );
+
+        return state;
+    }
+
+    function G1P(uint[2] memory p) internal returns (G1Point memory) {
+        return G1Point(p[0], p[1]);
+    }
+
+}
+
