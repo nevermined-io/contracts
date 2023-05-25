f308b0e6bb7ee1a8a6cc50590cdb7638e9560477
Sami Mäkelä <mrsmkl@gmail.com>
2023-05-25 13:57:13 +0300

    making shares

--- test/helpers/dkg.js
+++ test/helpers/dkg.js
@@ -18,16 +18,6 @@ async function round1(t, i, ctx) {
     // generate secrets
     const poly = Array(t).fill().map(_a => Fr.random())
 
-    function evalPoly(x) {
-        let xn = Fr.fromObject(1n)
-        let res = Fr.fromObject(0n)
-        for (const c of poly) {
-            res = Fr.add(res, Fr.mul(c, xn))
-            xn = Fr.mul(xn, Fr.fromObject(x))
-        }
-        return res
-    }
-
     function hash(lst) {
         return Fr.fromObject(BigInt(ethers.utils.solidityKeccak256(lst.map(a => 'uint256'), lst)))
     }
@@ -67,25 +57,87 @@ async function validate(obj) {
     const G1 = ffCurve.G1
     const G = G1.g
 
-    // generate secrets
-    const poly = Array(t).fill().map(_a => Fr.random())
-
     function hash(lst) {
         return Fr.fromObject(BigInt(ethers.utils.solidityKeccak256(lst.map(a => 'uint256'), lst)))
     }
-    const c = hash([obj.idx,obj.ctx].concat(toEvm(obj.commit[0])).concat(obj.proof_R))
+    const c = hash([obj.idx,obj.ctx].concat(obj.commit[0]).concat(obj.proof_R))
 
+    const R = G1.fromObject([BigInt(obj.proof_R[0]), BigInt(obj.proof_R[1])])
+    const mu = Fr.fromObject(obj.proof_mu)
+    const phi0 = G1.fromObject([BigInt(obj.commit[0][0]), BigInt(obj.commit[0][1])])
+
+    const check = G1.add(G1.timesFr(G, mu), G1.timesFr(phi0, Fr.neg(c)))
+
+    assert(G1.eq(R, check))
+
+}
+
+async function makeShares(obj, n) {
+    const ffCurve = await buildBn128()
+    const Fr = ffCurve.Fr
+    const G1 = ffCurve.G1
+    const G = G1.g
+
+    const poly = obj.secrets.map(a => Fr.fromObject(a))
+
+    function evalPoly(x) {
+        let xn = Fr.fromObject(1n)
+        let res = Fr.fromObject(0n)
+        for (const c of poly) {
+            res = Fr.add(res, Fr.mul(c, xn))
+            xn = Fr.mul(xn, Fr.fromObject(x))
+        }
+        return res
+    }
+
+    let shares = []
+    for (let i = 1; i <= n; i++) {
+        let share = evalPoly(i)
+        shares.push(Fr.toObject(share))
+    }
+
+    console.log("share", shares)
+
+    return shares
 }
 
 // full protocol
 
 async function proto(n, t, ctx) {
     let r1 = []
+    let commits = []
     for (let i = 0; i < n; i++) {
-        r1.push(await round1(t, i, ctx))
+        let obj = await round1(t, i, ctx)
+        r1.push(obj)
+        commits.push(obj.commits)
     }
 
     console.log(r1)
+
+    for (let obj of r1) {
+        await validate(obj)
+    }
+
+    console.log("validated proofs")
+
+    // generate and send shares p2p
+    let shares = []
+    for (let obj of r1) {
+        shares.push(await makeShares(obj, n))
+        obj.shares = []
+    }
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < n; j++) {
+            r1[i].shares.push(shares[j][i])
+        }
+    }
+    console.log("sent shares")
+
+    // each participant verifies shares and computes own key
+    for (let obj of r1) {
+        await verifyShares(obj, commits)
+    }
+
 
     process.exit(0)
 
