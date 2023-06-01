d10ebcbcdaa1b79c85bb53ce01269a84e57cddb4
Sami Mäkelä <mrsmkl@gmail.com>
2023-06-01 15:42:10 +0300

    lint

--- server/main.js
+++ server/main.js
@@ -1,8 +1,8 @@
-const express = require("express")
-const bodyParser = require("body-parser")
-const { JSONRPCServer, JSONRPCClient } = require("json-rpc-2.0")
+const express = require('express')
+const bodyParser = require('body-parser')
+const { JSONRPCServer, JSONRPCClient } = require('json-rpc-2.0')
 const { buildBn128 } = require('ffjavascript')
-const { ethers, BigNumber } = require('ethers')
+const { ethers } = require('ethers')
 const fetch = require('cross-fetch')
 const assert = require('assert')
 
@@ -10,38 +10,37 @@ function flatten(lst) {
     return lst.reduce((a, b) => a.concat(b), [])
 }
 
-let addresses = [
-    "http://localhost:23451",
-    "http://localhost:23452",
-    "http://localhost:23453",
-    "http://localhost:23454",
-    "http://localhost:23455",
+const addresses = [
+    'http://localhost:23451',
+    'http://localhost:23452',
+    'http://localhost:23453',
+    'http://localhost:23454',
+    'http://localhost:23455'
 ]
 
 async function makeServer(n, t, i, port) {
-
     // each address will have it's own client
     function makeClient(addr) {
         const client = new JSONRPCClient((jsonRPCRequest) =>
             fetch(`${addr}/json-rpc`, {
-                method: "POST",
+                method: 'POST',
                 headers: {
-                "content-type": "application/json",
+                    'content-type': 'application/json'
                 },
-                body: JSON.stringify(jsonRPCRequest),
+                body: JSON.stringify(jsonRPCRequest)
             }).then((response) => {
                 if (response.status === 200) {
                 // Use client.receive when you received a JSON-RPC response.
-                return response
-                    .json()
-                    .then((jsonRPCResponse) => client.receive(jsonRPCResponse));
+                    return response
+                        .json()
+                        .then((jsonRPCResponse) => client.receive(jsonRPCResponse))
                 } else if (jsonRPCRequest.id !== undefined) {
-                    return Promise.reject(new Error(response.statusText));
+                    return Promise.reject(new Error(response.statusText))
                 }
-        }))
+            }))
         return client
     }
-    let clients = addresses.map(makeClient)
+    const clients = addresses.map(makeClient)
 
     // crypto utils
     const ffCurve = await buildBn128()
@@ -59,7 +58,7 @@ async function makeServer(n, t, i, port) {
     }
 
     function fromEvm(arr) {
-        return G1.fromObject([BigInt(arr[0]),BigInt(arr[1])])
+        return G1.fromObject([BigInt(arr[0]), BigInt(arr[1])])
     }
 
     function toString(n) {
@@ -94,7 +93,7 @@ async function makeServer(n, t, i, port) {
             res = Fr.mul(res, a)
         }
         return res
-    } 
+    }
 
     // lagrange coefficient
     function coeff(ids, i) {
@@ -107,33 +106,33 @@ async function makeServer(n, t, i, port) {
         return res
     }
 
-    let obj = {
+    const obj = {
         idx: i,
         used: {},
-        shares: [],
+        shares: []
     }
 
     // methods needed for DKG
-    async function init_round1(params) {
-        let ctx = BigInt(params.ctx)
+    async function initRound1(params) {
+        const ctx = BigInt(params.ctx)
         if (obj.used[ctx]) {
-            return {error: "Context label used"}
+            return { error: 'Context label used' }
         }
         obj.used[ctx] = true
-        console.log("Initializing round 1")
+        console.log('Initializing round 1')
 
         // generate secrets
         const poly = Array(t).fill().map(_a => Fr.random())
-    
+
         // proof of knowledge for the secret (Schnorr sig)
         const k = Fr.random()
-    
+
         const R = G1.timesFr(G, k)
         const a0G = G1.timesFr(G, poly[0])
-        const c = hash([i,ctx].concat(toEvm(a0G)).concat(toEvm(R)))
+        const c = hash([i, ctx].concat(toEvm(a0G)).concat(toEvm(R)))
 
         const mu = Fr.add(k, Fr.mul(poly[0], c))
-    
+
         const phi = poly.map(a => G1.timesFr(G, a))
 
         obj.proof_mu = toString(mu)
@@ -143,110 +142,109 @@ async function makeServer(n, t, i, port) {
         obj.ctx = ctx
 
         // return commitments and proof
-        let resp = {
+        const resp = {
             proof_mu: obj.proof_mu,
             proof_R: obj.proof_R,
             commit: obj.commit,
             idx: obj.idx,
-            ctx: obj.ctx.toString(),
+            ctx: obj.ctx.toString()
         }
 
-        for (let c of clients) {
-            await c.request("validate_round1", resp)
+        for (const c of clients) {
+            await c.request('validate_round1', resp)
         }
 
-        return "ok"
+        return 'ok'
     }
 
-    let objs = []
+    const objs = []
     objs[i] = obj
 
     // validate message received from round 1
-    async function validate_round1(obj) {
+    async function validateRound1(obj) {
         // console.log(obj)
-        const c = hash([BigInt(obj.idx),BigInt(obj.ctx)].concat(obj.commit[0]).concat(obj.proof_R))
-    
+        const c = hash([BigInt(obj.idx), BigInt(obj.ctx)].concat(obj.commit[0]).concat(obj.proof_R))
+
         const R = fromEvm(obj.proof_R)
         const mu = fromString(obj.proof_mu)
         const phi0 = fromEvm(obj.commit[0])
-    
+
         const check = G1.add(G1.timesFr(G, mu), G1.timesFr(phi0, Fr.neg(c)))
-    
+
         assert(G1.eq(R, check))
-        console.log(i, "got valid round 1 message", obj.idx)
+        console.log(i, 'got valid round 1 message', obj.idx)
         objs[obj.idx] = obj
-        return "ok"
+        return 'ok'
     }
 
-    async function coordinate_round1({ctx}) {
-        for (let c of clients) {
-            await c.request("init_round1", {ctx})
+    async function coordinateRound1({ ctx }) {
+        for (const c of clients) {
+            await c.request('init_round1', { ctx })
         }
 
-        return "ok"
+        return 'ok'
     }
 
     function computeKey(commits) {
-        let pubkey = sumG1(commits.map(a => fromEvm(a[0])))
-    
-        console.log("got public key", toEvm(pubkey))
-    
-        return toEvm(pubkey)
-    }   
+        const pubkey = sumG1(commits.map(a => fromEvm(a[0])))
 
-    async function makeShares({commits}) {
+        console.log('got public key', toEvm(pubkey))
+
+        return toEvm(pubkey)
+    }
+
+    async function makeShares({ commits }) {
         obj.commits = commits
         const poly = obj.secrets.map(a => fromString(a))
-    
-        let shares = []
+
+        const shares = []
         for (let i = 1; i <= n; i++) {
-            let share = evalPoly(poly, i)
+            const share = evalPoly(poly, i)
             shares.push(toString(share))
         }
 
-        console.log("share", shares)
+        console.log('share', shares)
         obj.backup = shares
         for (let i = 0; i < clients.length; i++) {
-            let c = clients[i]
-            await c.request("set_share", {idx: obj.idx, share: shares[i]})
+            const c = clients[i]
+            await c.request('set_share', { idx: obj.idx, share: shares[i] })
         }
 
-        return "ok"
+        return 'ok'
     }
 
     async function verifyShares() {
-    
         const n = obj.shares.length
         const t = obj.secrets.length
-    
-        const idx = Fr.fromObject(BigInt(obj.idx+1))
-    
+
+        const idx = Fr.fromObject(BigInt(obj.idx + 1))
+
         let secret = Fr.fromObject(0n)
-    
+
         for (let l = 0; l < n; l++) {
-            let share = fromString(obj.shares[l])
-            let pub = G1.timesFr(G, share)
-            let asd = []
+            const share = fromString(obj.shares[l])
+            const pub = G1.timesFr(G, share)
+            const asd = []
             for (let k = 0; k < t; k++) {
-                let cp = fromEvm(obj.commits[l][k])
-                let exp = pow(idx, k)
+                const cp = fromEvm(obj.commits[l][k])
+                const exp = pow(idx, k)
                 // console.log("exp",Fr.toObject(exp))
                 asd.push(G1.timesFr(cp, exp))
             }
-            let check = sumG1(asd)
+            const check = sumG1(asd)
             // console.log("check", toEvm(check), "pub", toEvm(pub), obj.idx, n, t)
             assert(G1.eq(check, pub))
             secret = Fr.add(share, secret)
         }
-        console.log("found share", Fr.toObject(secret))
-    
+        console.log('found share', Fr.toObject(secret))
+
         obj.share = toString(secret)
         obj.pubkey = computeKey(obj.commits)
     }
 
     async function setShare(dta) {
         obj.shares[dta.idx] = dta.share
-        console.log("Member", obj.idx, "got share from", dta.idx)
+        console.log('Member', obj.idx, 'got share from', dta.idx)
         let flag = true
         for (let i = 0; i < n; i++) {
             if (!obj.shares[i]) flag = false
@@ -254,35 +252,35 @@ async function makeServer(n, t, i, port) {
         if (flag) {
             verifyShares()
         }
-        return "ok"
+        return 'ok'
     }
 
-    async function coordinate_round2({}) {
+    async function coordinateRound2() {
         // send commits
-        let commits = []
+        const commits = []
         for (let i = 0; i < n; i++) {
             if (!objs[i] || !objs[i].commit) {
-                console.log("Round 2 not finished: did not receive all commits", i)
+                console.log('Round 2 not finished: did not receive all commits', i)
             }
             commits.push(objs[i].commit)
         }
         computeKey(commits)
 
-        for (let c of clients) {
-            await c.request("make_shares", {commits})
+        for (const c of clients) {
+            await c.request('make_shares', { commits })
         }
 
-        return "ok"
+        return 'ok'
     }
 
     async function crypt(dta) {
-        let R = fromEvm(dta.key)
-        let res = G1.timesFr(R, fromString(obj.share))
+        const R = fromEvm(dta.key)
+        const res = G1.timesFr(R, fromString(obj.share))
         return toEvm(res)
     }
 
-    async function frost_round1(dta) {
-        let R = fromEvm(dta.key)
+    async function frostRound1(dta) {
+        const R = fromEvm(dta.key)
         const s1 = Fr.random()
         const s2 = Fr.random()
         const c1 = G1.timesFr(G, s1)
@@ -292,7 +290,7 @@ async function makeServer(n, t, i, port) {
         const pub1 = G1.timesFr(G, fromString(obj.share))
         const pub2 = G1.timesFr(R, fromString(obj.share))
         if (obj.s1) {
-            console.log("overriding secret!!!!!!!!!!!!!!!")
+            console.log('overriding secret!!!!!!!!!!!!!!!')
         }
         obj.s1 = toString(s1)
         obj.s2 = toString(s2)
@@ -305,7 +303,7 @@ async function makeServer(n, t, i, port) {
             pub2: toEvm(pub2),
             // TODO: remove these
             s1: toString(s1),
-            s2: toString(s2),
+            s2: toString(s2)
         }
     }
 
@@ -328,84 +326,81 @@ async function makeServer(n, t, i, port) {
         console.log('challenge', Fr.toObject(chal))
 
         return chal
-
     }
 
-    async function frost_round2(dta) {
-        let nonces = dta.nonces
+    async function frostRound2(dta) {
+        const nonces = dta.nonces
 
         const chal = computeChallenge(nonces, dta.label, dta.yG, dta.yR)
 
         // compute response
-        let a = nonces.find(a => a.idx == obj.idx)
-        // let num = nonces.findIndex(a => a.idx == obj.idx)
-        let s1 = fromString(obj.s1)
-        let s2 = fromString(obj.s2)
-        let share = fromString(obj.share)
-        console.log("coeff", obj.idx, ":", toString(coeff(dta.ids, obj.idx)))
-        let resp = Fr.add(s1, Fr.add(Fr.mul(s2, a.bind), Fr.neg(Fr.mul(coeff(dta.ids, obj.idx), Fr.mul(share, chal)))))
+        const a = nonces.find(a => a.idx === obj.idx)
+        const s1 = fromString(obj.s1)
+        const s2 = fromString(obj.s2)
+        const share = fromString(obj.share)
+        const resp = Fr.add(s1, Fr.add(Fr.mul(s2, a.bind), Fr.neg(Fr.mul(coeff(dta.ids, obj.idx), Fr.mul(share, chal)))))
         console.log('response', toString(resp))
         return toString(resp)
     }
 
     async function reencrypt(dta) {
-        let ids = dta.ids
-        
+        const ids = dta.ids
+
         const coeffs = ids.map(i => coeff(ids, i))
         console.log('coeffs', coeffs.map(a => Fr.toObject(a)))
 
         // network id
-        let yG = G1.fromObject([BigInt(obj.pubkey[0]),BigInt(obj.pubkey[1])])
+        const yG = G1.fromObject([BigInt(obj.pubkey[0]), BigInt(obj.pubkey[1])])
 
         // secret id
-        let xG = G1.fromObject([BigInt(dta.id[0]),BigInt(dta.id[1])])
+        const xG = G1.fromObject([BigInt(dta.id[0]), BigInt(dta.id[1])])
 
         // consumer id
-        let zG = G1.fromObject([BigInt(dta.consumer[0]),BigInt(dta.consumer[1])])
+        const zG = G1.fromObject([BigInt(dta.consumer[0]), BigInt(dta.consumer[1])])
 
         // reencrypt target
         const R = G1.add(xG, zG)
-        console.log("R", toEvm(R))
+        console.log('R', toEvm(R))
 
-        let parts = await Promise.all(coeffs.map(async (c,i) => G1.timesFr(fromEvm(await clients[ids[i]].request("crypt", {key: toEvm(R)})), c)))
-        let yR = sumG1(parts)
+        const parts = await Promise.all(coeffs.map(async (c, i) => G1.timesFr(fromEvm(await clients[ids[i]].request('crypt', { key: toEvm(R) })), c)))
+        const yR = sumG1(parts)
 
-        console.log("yR", toEvm(yR), "yG", toEvm(yG))
+        console.log('yR', toEvm(yR), 'yG', toEvm(yG))
 
         // frost round 1 (pre-process)
-        let nonces = await Promise.all(ids.map(async (idx, i) => {
-            let nonce = await clients[idx].request("frost_round1", {key: toEvm(R)})
+        const nonces = await Promise.all(ids.map(async (idx, i) => {
+            const nonce = await clients[idx].request('frost_round1', { key: toEvm(R) })
             nonce.idx = idx
             nonce.coeff = coeffs[i]
             return nonce
         }))
 
-        console.log("frost round 1")
+        console.log('frost round 1')
         console.log(JSON.stringify(nonces))
 
         // collect round 2 responses
-        for (let a of nonces) {
-            let req = {
+        for (const a of nonces) {
+            const req = {
                 ids,
                 key: toEvm(R),
                 nonces,
                 label: dta.label,
                 yG: toEvm(yG),
-                yR: toEvm(yR),
+                yR: toEvm(yR)
             }
-            a.resp = await clients[a.idx].request("frost_round2", req)
+            a.resp = await clients[a.idx].request('frost_round2', req)
         }
 
         // aggregating signatures
         const chal = computeChallenge(nonces, dta.label, toEvm(yG), toEvm(yR))
 
-        console.log("frost round 2")
+        console.log('frost round 2')
         // verify partial signatures
         nonces.forEach(a => {
             const resp1 = G1.timesFr(G, fromString(a.resp))
             const resp2 = G1.timesFr(R, fromString(a.resp))
-            let pub1 = fromEvm(a.pub1)
-            let pub2 = fromEvm(a.pub2)
+            const pub1 = fromEvm(a.pub1)
+            const pub2 = fromEvm(a.pub2)
             const committedResp1 = G1.add(a.commit1, G1.neg(G1.timesFr(pub1, Fr.mul(chal, a.coeff))))
             const committedResp2 = G1.add(a.commit2, G1.neg(G1.timesFr(pub2, Fr.mul(chal, a.coeff))))
             console.log('resp1', toEvm(resp1), 'should be', toEvm(committedResp1))
@@ -428,34 +423,33 @@ async function makeServer(n, t, i, port) {
             R: toEvm(R),
             yR: toEvm(yR),
             response: toString(rResp),
-            chal: toString(chal),
+            chal: toString(chal)
         }
-
     }
 
     const server = new JSONRPCServer()
 
-    server.addMethod("echo", ({ text }) => text)
-    server.addMethod("log", ({ message }) => console.log(message))
+    server.addMethod('echo', ({ text }) => text)
+    server.addMethod('log', ({ message }) => console.log(message))
 
-    server.addMethod("init_round1", init_round1)
-    server.addMethod("validate_round1", validate_round1)
-    server.addMethod("coordinate_round1", coordinate_round1)
-    server.addMethod("make_shares", makeShares)
-    server.addMethod("set_share", setShare)
-    server.addMethod("coordinate_round2", coordinate_round2)
+    server.addMethod('init_round1', initRound1)
+    server.addMethod('validate_round1', validateRound1)
+    server.addMethod('coordinate_round1', coordinateRound1)
+    server.addMethod('make_shares', makeShares)
+    server.addMethod('set_share', setShare)
+    server.addMethod('coordinate_round2', coordinateRound2)
 
-    server.addMethod("reencrypt", reencrypt)
-    server.addMethod("crypt", crypt)
-    server.addMethod("frost_round1", frost_round1)
-    server.addMethod("frost_round2", frost_round2)
+    server.addMethod('reencrypt', reencrypt)
+    server.addMethod('crypt', crypt)
+    server.addMethod('frost_round1', frostRound1)
+    server.addMethod('frost_round2', frostRound2)
 
-    server.addMethod("test_account", () => {
+    server.addMethod('test_account', () => {
         const x = Fr.random()
         const xG = G1.timesFr(G, x)
         return {
             secret: toString(x),
-            public: toEvm(xG),
+            public: toEvm(xG)
         }
     })
 
@@ -468,24 +462,24 @@ async function makeServer(n, t, i, port) {
     const app = express()
     app.use(bodyParser.json())
 
-    app.post("/json-rpc", (req, res) => {
-        const jsonRPCRequest = req.body;
+    app.post('/json-rpc', (req, res) => {
+        const jsonRPCRequest = req.body
         // server.receive takes a JSON-RPC request and returns a promise of a JSON-RPC response.
         // It can also receive an array of requests, in which case it may return an array of responses.
         // Alternatively, you can use server.receiveJSON, which takes JSON string as is (in this case req.body).
         server.receive(jsonRPCRequest).then((jsonRPCResponse) => {
             if (jsonRPCResponse) {
-                res.json(jsonRPCResponse);
+                res.json(jsonRPCResponse)
             } else {
                 // If response is absent, it was a JSON-RPC notification method.
                 // Respond with no content status (204).
-                res.sendStatus(204);
+                res.sendStatus(204)
             }
         })
     })
 
     app.listen(port)
-    console.log("running at port", port)
+    console.log('running at port', port)
 }
 
 makeServer(5, 3, 0, 23451)
--- test/helpers/dkg.js
+++ test/helpers/dkg.js
@@ -32,7 +32,7 @@ async function round1(t, i, ctx) {
 
     const R = G1.timesFr(G, k)
     const a0G = G1.timesFr(G, poly[0])
-    const c = hash([i,ctx].concat(toEvm(a0G)).concat(toEvm(R)))
+    const c = hash([i, ctx].concat(toEvm(a0G)).concat(toEvm(R)))
 
     const mu = Fr.add(k, Fr.mul(poly[0], c))
 
@@ -44,14 +44,12 @@ async function round1(t, i, ctx) {
         commit: phi.map(a => toEvm(a)),
         idx: i,
         secrets: poly.map(a => Fr.toObject(a)),
-        ctx,
+        ctx
     }
-
 }
 
 // validate signature
 async function validate(obj) {
-
     const ffCurve = await buildBn128()
     const Fr = ffCurve.Fr
     const G1 = ffCurve.G1
@@ -60,7 +58,7 @@ async function validate(obj) {
     function hash(lst) {
         return Fr.fromObject(BigInt(ethers.utils.solidityKeccak256(lst.map(a => 'uint256'), lst)))
     }
-    const c = hash([obj.idx,obj.ctx].concat(obj.commit[0]).concat(obj.proof_R))
+    const c = hash([obj.idx, obj.ctx].concat(obj.commit[0]).concat(obj.proof_R))
 
     const R = G1.fromObject([BigInt(obj.proof_R[0]), BigInt(obj.proof_R[1])])
     const mu = Fr.fromObject(obj.proof_mu)
@@ -69,14 +67,11 @@ async function validate(obj) {
     const check = G1.add(G1.timesFr(G, mu), G1.timesFr(phi0, Fr.neg(c)))
 
     assert(G1.eq(R, check))
-
 }
 
 async function makeShares(obj, n) {
     const ffCurve = await buildBn128()
     const Fr = ffCurve.Fr
-    const G1 = ffCurve.G1
-    const G = G1.g
 
     const poly = obj.secrets.map(a => Fr.fromObject(a))
 
@@ -90,22 +85,20 @@ async function makeShares(obj, n) {
         return res
     }
 
-    let shares = []
+    const shares = []
     for (let i = 1; i <= n; i++) {
-        let share = evalPoly(i)
+        const share = evalPoly(i)
         shares.push(Fr.toObject(share))
     }
 
-    console.log("share", shares)
+    console.log('share', shares)
 
     return shares
 }
 
 async function computeKey(commits) {
     const ffCurve = await buildBn128()
-    const Fr = ffCurve.Fr
     const G1 = ffCurve.G1
-    const G = G1.g
 
     function sumG1(lst) {
         return lst.reduce((a, b) => G1.add(a, b), G1.fromObject([0n, 0n]))
@@ -116,9 +109,9 @@ async function computeKey(commits) {
         return [obj[0].toString(10), obj[1].toString(10)]
     }
 
-    let pubkey = sumG1(commits.map(a => G1.fromObject([BigInt(a[0][0]), BigInt(a[0][1])])))
+    const pubkey = sumG1(commits.map(a => G1.fromObject([BigInt(a[0][0]), BigInt(a[0][1])])))
 
-    console.log("got public key", toEvm(pubkey))
+    console.log('got public key', toEvm(pubkey))
 
     return toEvm(pubkey)
 }
@@ -133,43 +126,38 @@ async function verifyShares(obj, commits) {
         return lst.reduce((a, b) => G1.add(a, b), G1.fromObject([0n, 0n]))
     }
 
-    function toEvm(p) {
-        const obj = G1.toObject(G1.toAffine(p))
-        return [obj[0].toString(10), obj[1].toString(10)]
-    }
-
     function pow(a, n) {
         let res = Fr.fromObject(1n)
         for (let i = 0; i < n; i++) {
             res = Fr.mul(res, a)
         }
         return res
-    } 
+    }
 
     const n = obj.shares.length
     const t = obj.secrets.length
 
-    const idx = Fr.fromObject(BigInt(obj.idx+1))
+    const idx = Fr.fromObject(BigInt(obj.idx + 1))
 
     let secret = Fr.fromObject(0n)
 
     for (let l = 0; l < n; l++) {
-        let share = Fr.fromObject(obj.shares[l])
-        let pub = G1.timesFr(G, share)
-        let asd = []
+        const share = Fr.fromObject(obj.shares[l])
+        const pub = G1.timesFr(G, share)
+        const asd = []
         for (let k = 0; k < t; k++) {
-            let c = commits[l][k]
-            let cp = G1.fromObject([BigInt(c[0]),BigInt(c[1])])
-            let exp = pow(idx, k)
+            const c = commits[l][k]
+            const cp = G1.fromObject([BigInt(c[0]), BigInt(c[1])])
+            const exp = pow(idx, k)
             // console.log("exp",Fr.toObject(exp))
             asd.push(G1.timesFr(cp, exp))
         }
-        let check = sumG1(asd)
+        const check = sumG1(asd)
         // console.log("check", toEvm(check), "pub", toEvm(pub), obj.idx)
         assert(G1.eq(check, pub))
         secret = Fr.add(share, secret)
     }
-    console.log("found share", Fr.toObject(secret))
+    console.log('found share', Fr.toObject(secret))
 
     obj.share = secret
 }
@@ -177,25 +165,25 @@ async function verifyShares(obj, commits) {
 // full protocol
 
 async function proto(n, t, ctx) {
-    let r1 = []
-    let commits = []
+    const r1 = []
+    const commits = []
     for (let i = 0; i < n; i++) {
-        let obj = await round1(t, i, ctx)
+        const obj = await round1(t, i, ctx)
         r1.push(obj)
         commits.push(obj.commit)
     }
 
     console.log(r1)
 
-    for (let obj of r1) {
+    for (const obj of r1) {
         await validate(obj)
     }
 
-    console.log("validated proofs")
+    console.log('validated proofs')
 
     // generate and send shares p2p
-    let shares = []
-    for (let obj of r1) {
+    const shares = []
+    for (const obj of r1) {
         shares.push(await makeShares(obj, n))
         obj.shares = []
     }
@@ -204,20 +192,20 @@ async function proto(n, t, ctx) {
             r1[i].shares.push(shares[j][i])
         }
     }
-    console.log("sent shares")
+    console.log('sent shares')
 
     // each participant verifies shares and computes own key
-    for (let obj of r1) {
+    for (const obj of r1) {
         await verifyShares(obj, commits)
     }
 
-    console.log("verified shares")
+    console.log('verified shares')
 
-    let pubkey = await computeKey(commits)
+    const pubkey = await computeKey(commits)
 
     return {
         shares: r1.map(a => a.share),
-        pubkey,
+        pubkey
     }
 }
 
@@ -226,7 +214,7 @@ async function frostDLEQ(n, t) {
     const Fr = ffCurve.Fr
 
     // generate secrets
-    const { shares } = await proto(n,t,23782732837n)
+    const { shares } = await proto(n, t, 23782732837n)
 
     const ids = [2, 3, 4]
 
@@ -386,4 +374,3 @@ async function frostDLEQ(n, t) {
 }
 
 frostDLEQ(5, 3)
-
