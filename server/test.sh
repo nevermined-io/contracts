bc8c20d42efbbada74ed9b1a3c0ae0ee7080c1cf
Sami Mäkelä <mrsmkl@gmail.com>
2023-06-29 13:30:04 +0300

    adding test

--- server/test.sh
+++ server/test.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+npx hardhat node --port 8545 &
+
+sleep 10
+
+npx hardhat run ./scripts/deploy/deployContractsWrapper.js --network testing
+
 node server/main.js &
 
 sleep 5
@@ -13,7 +19,13 @@ npx hardhat run server/setup.js --network testing
 
 sleep 5
 
-curl http://localhost:23451/json-rpc -s -X POST -H "Content-Type: application/json" --data '{"jsonrpc":"2.0","method":"listen","params":{},"id":12}'
+curl http://localhost:23451/json-rpc -s -X POST -H "Content-Type: application/json" --data '{"jsonrpc":"2.0","method":"listen","params":{},"id":12}' > test1.res.json
+
+diff test1.res.json server/test2.json || exit 1
+
+curl http://localhost:23451/json-rpc -s -X POST -H "Content-Type: application/json" --data '{"jsonrpc":"2.0","method":"setup","params":{},"id":12}' > test2.res.json
+
+diff test2.res.json server/test2.json || exit 1
 
 sleep 10
 
