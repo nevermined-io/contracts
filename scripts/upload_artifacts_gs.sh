fffdae7d0a4f51b0d5ac8255e712f41f58905b16
Aitor <1726644+aaitor@users.noreply.github.com>
2024-05-06 10:26:40 +0200

    feat: peaq mainnet config

--- scripts/upload_artifacts_gs.sh
+++ scripts/upload_artifacts_gs.sh
@@ -46,6 +46,7 @@ NETWORKS_MAP=(
   ["arbitrum-one"]="42161"
   ["celo-alfajores"]="44787"
   ["hyperspace"]="3141"
+  ["peaq-mainnet"]="3338"
   ["peaq-agung"]="9990"
   ["celo"]="42220"
   ["mumbai"]="80001"
